7acc32025c79004ecd2cd0ca323261366083288d
wen <36833420+zhangshanwen@users.noreply.github.com>
2024-07-12 17:47:09 +0800

    1

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      image="https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg"
+      theme="dark"
+      permanent
+    >
+      <template v-slot:prepend>
+        <div class="pa-2">
+          <v-btn block>
+            Show Dialog1
+          </v-btn>
+        </div>
+      </template>
+      <v-list nav>
+        <v-list-item v-for="i in 9">
+          <div class="pa-2">
+            <v-btn> Show Dialog List{{ i }}</v-btn>
+          </div>
+        </v-list-item>
+
+      </v-list>
+      <template v-slot:append>
+        <div class="pa-2">
+          <v-btn block>
+            Show Dialog2
+          </v-btn>
+        </div>
+      </template>
+
+    </v-navigation-drawer>
+    <v-main>
+      <v-app-bar location="top" class="d-flex align-center">
+        <v-btn color="primary" variant="outlined"> Show Dialog Top</v-btn>
+      </v-app-bar>
+      <v-app-bar location="bottom" class="d-flex align-center">
+        <v-btn color="primary" variant="outlined"> Show Dialog Bottom</v-btn>
+      </v-app-bar>
+      <v-card class="h-100">
+        <iframe src="/iframe.html" class="w-100 h-100"></iframe>
+      </v-card>
+    </v-main>
+    <local-dialog v-model="dialog"></local-dialog>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+import LocalDialog from "@/components/LocalDialog.vue";
+import {ref} from "vue"
+
+const dialog = ref(false)
+</script>
+
+<style></style>
---
+++ src/components/LocalDialog.vue
@@ -0,0 +1,22 @@
+<template>
+
+  <v-dialog v-model="props.modelValue" :width="height" :height="height">
+    <v-card>
+      <h1> hello world</h1>
+
+    </v-card>
+
+  </v-dialog>
+
+</template>
+
+<script setup>
+
+const props = defineProps({
+
+  modelValue: {type: Boolean},
+  width: {type: Number, default: 200},
+  height: {type: Number, default: 200},
+})
+
+</script>
